72b6fda5b6826c0274c1a8aab1ff87b6f34115af
hyeokjuzzz <140710676+hyeokjuzzz@users.noreply.github.com>
2023-12-06 10:53:24 +0900

    Update complete.py

--- PI4/complete.py
+++ PI4/complete.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+
 from enum import Enum
 import os
 from time import sleep
@@ -8,18 +8,9 @@ import sys, os
 from enum import Enum
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from common.motor import Motor, GuideMotorStep
-from common.sensor import Sensor
+from common.irSensor import InfraredSensor
+from common.lightSensor import LightSensor
 from common.server_communication import ServerComm
-
-
-
-# 현재 스크립트 파일의 디렉토리 경로
-#current_path = os.path.dirname(__file__)
-# 외부 폴더의 경로 지정 (예: /home/pi/external_folder)
-#external_path = os.path.join(current_path, '/home/admin/test/common')
-# sys.path에 외부 폴더 경로 추가
-#sys.path.append(external_path)#
-
 
 class Step( Enum ) :    #각 스텝별 이름, 동사형으로 지을것, 무엇을 하는 스텝인지 알 수 있는 네이밍
      
@@ -38,27 +29,28 @@ pass_or_fail2 = ''
 
 # GPIO 핀 번호 설정
 LIGHT_SENSOR_PIN = 6
-IR_SENSOR_PIN = 7
+LIGHT_IR_SENSOR_PIN = 7
 SERVO_MOTOR_1_PIN = 17
 SERVO_MOTOR_2_PIN = 18
 
 currnet_step = Step.start   #기본설정
 running = True  
-sensor = Sensor()   #센서 참조
+ir_sensor = InfraredSensor( LIGHT_IR_SENSOR_PIN )   #센서 참조
+light_sensor = LightSensor( LIGHT_SENSOR_PIN )
 server_comm = ServerComm()  #서버참조
-svmotor_1 = Motor(SERVO_MOTOR_1_PIN) # 주파수 50Hz
-svmotor_2 = Motor(SERVO_MOTOR_2_PIN)
-motor = Motor # DC모터
+servo_motor_1 = Motor().servo_init(SERVO_MOTOR_1_PIN) # 주파수 50Hz
+servo_motor_2 = Motor().servo_init(SERVO_MOTOR_2_PIN)
+dc_motor = Motor().dc_init(1,2,3) # DC모터
 
 while running:
     print( "running : " + str( running ) )# 디버깅확인용
     sleep( 0.1 )
-    FOURTH_IR = sensor.get_ir_sensor( IR_SENSOR_PIN )  ##마지막에 활용
+    FOURTH_IR = ir_sensor.measure_ir()
     match current_step :
         case Step.start: 
             print( Step.start )
-            svmotor_2.doGuideMotor( GuideMotorStep.stop )
-            svmotor_1.doGuideMotor( GuideMotorStep.stop )
+            servo_motor_2.doGuideMotor( GuideMotorStep.stop )
+            servo_motor_1.doGuideMotor( GuideMotorStep.stop )
             #시작하기전에 검사할것들: 통신확인여부, 모터정렬, 센서 검수
             current_step = Step.fourth_part_irsensor_post #다음스텝으로 이동sd
 
@@ -93,7 +85,7 @@ while running:
         case Step.fourth_part_sensor_measure_and_endpost:
             print( Step.fourth_part_sensor_measure_and_endpost)
             #조도센서값을 판단
-            light_value = sensor.get_light_sensor()
+            light_value = light_sensor.measure_light()
             #조도센서 값을 서버에 전송
             end_light = server_comm.metalWiringEnd(light_value)
             if(end_light == "fail"):
@@ -111,13 +103,13 @@ while running:
 
         case Step.move_servo:
             print(Step.move_servo)
-            svmotor_1.doGuideMotor(pass_or_fail1)
-            svmotor_2.doGuideMotor(pass_or_fail2)  
+            servo_motor_1.doGuideMotor(pass_or_fail1)
+            servo_motor_2.doGuideMotor(pass_or_fail2)  
             current_step = Step.go_rail_next_1
 
         case Step.go_rail_next_1:
             print(Step.go_rail_next_1)
-            motor.doConveyor()  # 모터를 구동시킴
+            dc_motor.doConveyor()  # 모터를 구동시킴
             current_step = Step.stop_rail_1
 
         case Step.stop_rail_1:
@@ -126,7 +118,7 @@ while running:
             else:
                 sleep(8) 
 
-            motor.stopConveyor()  # 모터를 정지시킴
+            dc_motor.stopConveyor()  # 모터를 정지시킴
             current_step = Step.end_time
 
         case Step.end_time:
